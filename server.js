dfed7dbf51ce0f264f2965969c1cae225da5b713
Jason <jajones414@gmail.com>
2022-12-03 12:55:56 -0600

    Added server.js file.

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const sequelize = require('./config/connection')
+const express = require('express')
+const routes = require('./controllers')
+const { engine } = require('express-handlebars')
+const app = express()
+const PORT = process.env.PORT || 3001
 
-require('./models')
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-sequelize.sync({force: true})
+app.use(express.static('public'))
+app.use(express.json())
+app.use(routes)
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`server listening @ http://localhost:${PORT}`)
+    })
+})
+
 
